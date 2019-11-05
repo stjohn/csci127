5779dbb2fc179707a3236e08b96cec867586ed72
michniks2001 <57420719+michniks2001@users.noreply.github.com>
2019-11-05 18:42:57 -0500

    Update numsConvert.py

--- numsConvert.py
+++ numsConvert.py
@@ -1,24 +1,39 @@
 #CSci 127 Teaching Staff
 #February 2019
 #A program that uses functions to print out months.
-#Modified by:  ADD YOUR NAME HERE
+#Modified by: Samuel Michnik
 
 def num2string(num):
-     """
-     Takes as input a number, num, and
-     returns the corresponding name as a string.
-     Examples: num2string(0) returns "zero", num2string(1)returns "one"
-     Assumes that input is an integer ranging from 0 to 9
-     """
+     
+     ##Takes as input a number, num, and
+     ##returns the corresponding name as a string.
+     ##Examples: num2string(0) returns "zero", num2string(1)returns "one"
+     ##Assumes that input is an integer ranging from 0 to 9
+     
      
      numString = ""
-
-     ###################################
-     ### FILL IN YOUR CODE HERE      ###
-     ### Other than your name above, ###
-     ### this is the only section    ###
-     ### you change in this program. ###
-     ###################################
+     if num == 0:
+          numString = 'zero'
+          elif num == 1:
+               numString = 'one'
+               elif num == 2:
+                    numString = 'two'
+                    elif num == 3:
+                         numString = 'three'
+                         elif num == 4:
+                              numString = 'four'
+                              elif num == 5:
+                                   numString = 'five'
+                                   elif num == 6:
+                                        numString = 'six'
+                                        elif num == 7:
+                                             numString = 'seven'
+                                             elif num == 8:
+                                                  numString = 'eight'
+       
+     else:
+          numString = 'nine'
+         
 
      return(numString)
 
