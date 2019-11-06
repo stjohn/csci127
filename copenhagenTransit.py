ec833550b1b2ab7e22f05252097403967bf1de27
michniks2001 <57420719+michniks2001@users.noreply.github.com>
2019-11-06 13:44:36 -0500

    Update copenhagenTransit.py

--- copenhagenTransit.py
+++ copenhagenTransit.py
@@ -4,26 +4,40 @@
 #Modified by:  ADD YOUR NAME HERE
 
 def computeFare(zone, ticketType):
-     """
-     Takes as two parameters: the zone and the ticket type.
-     Returns the Copenhagen Transit fare, as follows:
+     
+     #Takes as two parameters: the zone and the ticket type.
+     #Returns the Copenhagen Transit fare, as follows:
 
-     If the zone is 2 or smaller and the ticket type is "adult", the fare is 23.
-     If the zone is 2 or smaller and the ticket type is "child", the fare is 11.5.
-     If the zone is 3 and the ticket type is "adult", the fare is 34.5.
-     If the zone is 3 or 4 and the ticket type is "child", the fare is 23.
-     If the zone is 4 and the ticket type is "adult", the fare is 46.
-     If the zone is greater than 4, return a negative number (since your calculator does not handle inputs that high).
-     """
+     #If the zone is 2 or smaller and the ticket type is "adult", the fare is 23.
+     #If the zone is 2 or smaller and the ticket type is "child", the fare is 11.5.
+     #If the zone is 3 and the ticket type is "adult", the fare is 34.5.
+     #If the zone is 3 or 4 and the ticket type is "child", the fare is 23.
+     #If the zone is 4 and the ticket type is "adult", the fare is 46.
+     #If the zone is greater than 4, return a negative number (since your calculator does not handle inputs that high).
+     
      
      fare = 0
      
-     ###################################
-     ### FILL IN YOUR CODE HERE      ###
-     ### Other than your name above, ###
-     ### this is the only section    ###
-     ### you change in this program. ###
-     ###################################
+     if zone <= 2:
+          if ticketType == 'adult':
+               fare = 23
+          elif ticketType == 'child':
+               fare = 11.5
+     
+     if zone == 3:
+          if ticketType == 'adult':
+               fare = 34.5
+          if ticketType == 'child':
+               fare == 23
+     
+     if zone == 4:
+          if ticketType == 'adult':
+               fare = 46
+          if ticketType == 'child':
+               fare = 23
+               
+     if zone > 4:
+          fare = -2
 
      return(fare)
 
